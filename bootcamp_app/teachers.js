a65c01778effdeffe5b3e9e4e2ca545ce3946105
jchanpark <junpark4936@gamil.com>
2022-04-09 23:15:13 -0400

    SQL Injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+const values = [`${cohortName}`]
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`
+
+pool.query(queryString, values)
 .then(res => {
   // console.log(res.rows);
   res.rows.forEach(row => {
